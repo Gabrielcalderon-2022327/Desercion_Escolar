39146daf2a0697cddb3f036344cf4f0f01894827
GabrielCalderon2022327 <gcalderon-2022327@kinal.edu.gt>
2026-02-18 08:26:34 -0600

    feat: Se agrego el proyecto de Spring boot

---
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/ProyectoDesercionApplication.java
@@ -0,0 +1,13 @@
+package com.scrum.ProyectoDesercion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDesercionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDesercionApplication.class, args);
+	}
+
+}
