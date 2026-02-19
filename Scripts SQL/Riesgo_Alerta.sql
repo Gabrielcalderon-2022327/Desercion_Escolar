ac8bc49ba0fff53591747c7c63c88b1188462018
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-18 20:35:39 -0600

    feat: Se agregaron los procedimientos almacenados para las entidades Riesgo y Alerta

--- Scripts SQL/Riesgo_Alerta.sql
+++ Scripts SQL/Riesgo_Alerta.sql
@@ -19,3 +19,121 @@ create table Alerta (
     fk_id_riesgo int not null,
 	constraint foreign key (fk_id_riesgo) references Riesgo(id_riesgo)
 );
+
+
+-- RIESGO  
+
+-- Agregar 
+delimiter $$
+create procedure sp_agregar_riesgo(
+    in p_nivel varchar(45),
+    in p_descripcion varchar(200),
+    in p_id_estudiante int
+)
+begin
+    insert into Riesgo(nivel_riesgo, descripcion_riesgo, fk_id_estudiante)
+    values (p_nivel, p_descripcion, p_id_estudiante);
+end $$
+delimiter ;
+
+-- Editar
+delimiter $$    
+create procedure sp_editar_riesgo(
+    in p_id int,
+    in p_nivel varchar(45),
+    in p_descripcion varchar(200),
+    in p_id_estudiante int
+)
+begin
+    update Riesgo
+    set nivel_riesgo = p_nivel,
+        descripcion_riesgo = p_descripcion,
+        fk_id_estudiante = p_id_estudiante
+    where id_riesgo = p_id;
+end $$
+delimiter ;
+
+-- Eliminar
+delimiter $$
+create procedure sp_eliminar_riesgo(in p_id int)
+begin
+    delete from Riesgo where id_riesgo = p_id;
+end $$
+delimiter ;
+
+-- Listar
+delimiter $$    
+create procedure sp_listar_riesgo()
+begin
+    
+    select * from Riesgo;
+end $$
+delimiter ; 
+
+-- Buscar
+delimiter $$
+create procedure sp_buscar_riesgo(in p_id int)
+begin
+	select * from Riesgo where id_riesgo = p_id;
+end $$
+delimiter ;
+     
+-- ALERTA 
+
+-- Agregar
+delimiter $$
+create procedure sp_agregar_alerta(
+    in p_fecha date,
+    in p_tipo varchar(45),
+    in p_incidente varchar(250),
+    in p_id_riesgo int
+)
+begin
+    insert into Alerta(fecha_alerta, tipo_alerta, incidente_alerta, fk_id_riesgo)
+    values (p_fecha, p_tipo, p_incidente, p_id_riesgo);
+end $$
+delimiter ;
+
+-- Editar
+delimiter $$
+create procedure sp_editar_alerta(
+    in p_id int,
+    in p_fecha date,
+    in p_tipo varchar(45),
+    in p_incidente varchar(250),
+    in p_id_riesgo int
+)
+begin
+    update Alerta
+    set fecha_alerta = p_fecha,
+        tipo_alerta = p_tipo,
+        incidente_alerta = p_incidente,
+        fk_id_riesgo = p_id_riesgo
+    where id_alerta = p_id;
+end $$
+delimiter ;
+
+-- Eliminar
+delimiter $$
+create procedure sp_eliminar_alerta(in p_id int)
+begin
+    delete from Alerta where id_alerta = p_id;
+end $$
+delimiter ;
+     
+-- Listar
+delimiter $$
+create procedure sp_listar_alerta()
+begin
+    
+    select * from Alerta;
+end $$
+delimiter ;
+
+-- Buscar
+delimiter $$
+create procedure sp_buscar_alerta(in p_id int)
+begin
+	select * from Alerta where id_alerta = p_id;
+end $$
+delimiter ;
