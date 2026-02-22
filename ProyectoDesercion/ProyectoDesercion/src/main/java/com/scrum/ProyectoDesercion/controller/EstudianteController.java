01a9a31e4d883db58062686c6aaea937d6463785
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-02-22 14:02:55 -0600

    refactor: Se optimizaron las excepciones

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/EstudianteController.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/EstudianteController.java
@@ -1,7 +1,9 @@
 package com.scrum.ProyectoDesercion.controller;
 
 import com.scrum.ProyectoDesercion.entity.Estudiante;
+import com.scrum.ProyectoDesercion.exception.ResourceNotFoundException;
 import com.scrum.ProyectoDesercion.service.EstudianteService;
+import com.scrum.ProyectoDesercion.validator.EstudianteValidator;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +16,11 @@ import java.util.Map;
 @RequestMapping("/api/estudiantes")
 public class EstudianteController {
     private final EstudianteService service;
+    private final EstudianteValidator validator;
 
-    public EstudianteController(EstudianteService service) {
+    public EstudianteController(EstudianteService service, EstudianteValidator validator) {
         this.service = service;
+        this.validator = validator;
     }
 
     @GetMapping
@@ -26,49 +30,27 @@ public class EstudianteController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getEstudianteById(@PathVariable Integer id){
-        try {
-            Estudiante searchedEstudiante = service.getEstudianteById(id);
-            return new ResponseEntity<>(searchedEstudiante, HttpStatus.OK);
-        } catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(Map.of("Error", e.getMessage()));
-        } catch (RuntimeException e){
-            return ResponseEntity.notFound().header("Error", e.getMessage()).build();
-        }
+        Estudiante searchedEstudiante = service.getEstudianteById(id);
+        return new ResponseEntity<>(searchedEstudiante, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Object> createEstudiante(@Valid @RequestBody Estudiante estudiante){
-        try {
-            Estudiante createdEstudiante = service.saveEstudiante(estudiante);
-            return new ResponseEntity<>(createdEstudiante, HttpStatus.CREATED);
-        } catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(Map.of("Error", e.getMessage()));
-        } catch (RuntimeException e){
-            return ResponseEntity.notFound().header("Error", e.getMessage()).build();
-        }
+        validator.validar(estudiante);
+        Estudiante createdEstudiante = service.saveEstudiante(estudiante);
+        return new ResponseEntity<>(createdEstudiante, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateEstudiante(@Valid @RequestBody Estudiante estudiante, @PathVariable Integer id){
-        try {
-            Estudiante updatedEstudiante = service.updateEstudiante(id, estudiante);
-            return new ResponseEntity<>(updatedEstudiante, HttpStatus.OK);
-        } catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(Map.of("Error", e.getMessage()));
-        } catch (RuntimeException e){
-            return ResponseEntity.notFound().header("Error", e.getMessage()).build();
-        }
+        validator.validar(estudiante);
+        Estudiante updatedEstudiante = service.updateEstudiante(id, estudiante);
+        return new ResponseEntity<>(updatedEstudiante, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteEstudiante(@PathVariable Integer id){
-        try {
-            service.deleteEstudiante(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().body(Map.of("Error", e.getMessage()));
-        } catch (RuntimeException e){
-            return ResponseEntity.notFound().header("Error", e.getMessage()).build();
-        }
+        service.deleteEstudiante(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
