7f016e34ee35083f4b2972414a04a2b3745c1bc1
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-02-22 11:16:28 -0600

    add: Se creo la Interface UsuarioRepository en la capa repository

---
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.scrum.ProyectoDesercion.repository;
+import com.scrum.ProyectoDesercion.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+
+}
