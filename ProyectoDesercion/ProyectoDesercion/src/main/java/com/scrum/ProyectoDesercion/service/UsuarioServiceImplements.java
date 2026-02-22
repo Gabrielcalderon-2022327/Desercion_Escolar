b0cec3d73795085383c0c7de5b899f10ae7b3b17
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-02-22 12:22:29 -0600

    add: Se agrego la clase UsuarioServiceImplements en la capa service

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/service/UsuarioServiceImplements.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/service/UsuarioServiceImplements.java
@@ -1,4 +1,49 @@
 package com.scrum.ProyectoDesercion.service;
 
-public class UsuarioServiceImplements {
+import com.scrum.ProyectoDesercion.entity.Usuario;
+import com.scrum.ProyectoDesercion.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UsuarioServiceImplements implements UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImplements(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public List<Usuario> getAllUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Usuario getUsuarioById(Integer id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Usuario saveUsuario(Usuario usuario) throws RuntimeException {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Usuario updateUsuario(Integer id, Usuario usuario) {
+        Usuario usuario1 = usuarioRepository.findById(id).orElse(null);
+        if (usuario1 != null) {
+            usuario1.setCorreoUsuario(usuario.getCorreoUsuario());
+            usuario1.setContraUsuario(usuario.getContraUsuario());
+            usuario1.setRolUsuario(usuario.getRolUsuario());
+            usuario1.setCreacionUsuario(usuario.getCreacionUsuario());
+        } else {
+            throw new RuntimeException("Usuario no encontrado");
+        }
+        return usuarioRepository.save(usuario1);
+    }
+
+    public void deleteUsuario(Integer id) {
+        usuarioRepository.deleteById(id);
+    }
 }
+
