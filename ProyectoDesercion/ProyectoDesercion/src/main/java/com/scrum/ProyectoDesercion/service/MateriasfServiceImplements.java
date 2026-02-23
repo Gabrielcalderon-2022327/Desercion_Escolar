fcc2dd00e2e4fdac15d6b9f98fd865a03ec7e354
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-02-23 14:36:02 -0600

    add Se agrego MateriasfServiceImplements en la capa service

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/service/MateriasfServiceImplements.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/service/MateriasfServiceImplements.java
@@ -1,4 +1,46 @@
 package com.scrum.ProyectoDesercion.service;
+import com.scrum.ProyectoDesercion.entity.MateriasF;
+import com.scrum.ProyectoDesercion.repository.MateriasfRepository;
+import org.springframework.stereotype.Service;
 
-public class MateriasfServiceImplements {
+import java.util.List;
+public class MateriasfServiceImplements implements MateriasfService{
+    private final MateriasfRepository materiasfRepository;
+
+    public MateriasfServiceImplements(MateriasfRepository materiasfRepository) { this.materiasfRepository = materiasfRepository; }
+
+    @Override
+    public List<MateriasF> getAllMateriasF() {
+        return materiasfRepository.findAll();
+    }
+
+    @Override
+    public MateriasF getMateriasFById(Integer id) {
+        return materiasfRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public MateriasF saveMateriasF(MateriasF materiasf) throws RuntimeException {
+        return materiasfRepository.save(materiasf);
+    }
+
+    @Override
+    public MateriasF updateMateriasF(Integer id, MateriasF materiasf) {
+        MateriasF materiasF1 = materiasfRepository.findById(id).orElse(null);
+        if (materiasF1 != null) {
+            materiasF1.setNombreMateriaF(materiasf.getNombreMateriaF());
+            materiasF1.setDescripcionMateriaF(materiasf.getDescripcionMateriaF());
+            materiasF1.setFechaAlertaMateriaF(materiasf.getFechaAlertaMateriaF());
+            materiasF1.setIdMaestro(materiasf.getIdMaestro());
+            materiasF1.setIdEstudiante(materiasf.getIdEstudiante());
+        } else {
+            throw new RuntimeException("Materia no encontrado");
+        }
+        return materiasfRepository.save(materiasF1);
+    }
+
+    public void deleteMateriasF(Integer id) {
+        materiasfRepository.deleteById(id);
+    }
 }
+
