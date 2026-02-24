1cffc217e4e5986da381b392719e52356da75782
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-23 21:43:48 -0600

    fix: Se corrigio error, se agrego @Repository en la interfaz

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/repository/GradoRepository.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/repository/GradoRepository.java
@@ -2,6 +2,8 @@ package com.scrum.ProyectoDesercion.repository;
 
 import com.scrum.ProyectoDesercion.entity.Grado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GradoRepository extends JpaRepository<Grado,Integer> {
 }
