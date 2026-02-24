2093ff6b4fb13ab3e3b07669f4ac08acfe04bbe4
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-23 21:37:06 -0600

    feat: Se agrego el repository de la entidad Grado

---
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/repository/GradoRepository.java
@@ -0,0 +1,7 @@
+package com.scrum.ProyectoDesercion.repository;
+
+import com.scrum.ProyectoDesercion.entity.Grado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GradoRepository extends JpaRepository<Grado,Integer> {
+}
