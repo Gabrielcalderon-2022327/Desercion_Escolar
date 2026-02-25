1bcb45e292545bd76dd4624d5c0e898ef2f44efd
Jaquino-2025171 <jaquino-2025171@kinal.edu.gt>
2026-02-25 09:08:18 -0600

    feat: se agrego la logica de la clase Alerta

---
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/entity/Alerta.java
@@ -0,0 +1,71 @@
+package com.scrum.ProyectoDesercion.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table (name = "Alerta")
+
+public class Alerta {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+
+@Column(name = "id_empleado")
+private Integer idAlerta;
+
+@Column(name = "fecha_alerta")
+@NotNull
+@Past(message = "error la fecha no puede ser futura")
+private LocalDate fechaAlerta;
+
+@Column(name = "tipo_alerta")
+@NotBlank (message = " el tipo de alerta no puede estar vacio")
+private String tipoAlerta;
+
+@Column(name = "incidente_alerta")
+@NotBlank(message = "el incidente de alerta no puede estar vacio")
+private String incidenteAlerta;
+
+
+// getter y setters
+
+
+    public Integer getIdAlerta() {
+        return idAlerta;
+    }
+
+    public void setIdAlerta(Integer idAlerta) {
+        this.idAlerta = idAlerta;
+    }
+
+    public Date getFecha_alerta() {
+        return fechaAlerta;
+    }
+
+    public void setFecha_alerta(Date fecha_alerta) {
+        this.fechaAlerta = fecha_alerta;
+    }
+
+    public String getTipo_alerta() {
+        return tipoAlerta;
+    }
+
+    public void setTipo_alerta(String tipo_alerta) {
+        this.tipoAlerta = tipo_alerta;
+    }
+
+    public String getIncidente_alerta() {
+        return incidenteAlerta;
+    }
+
+    public void setIncidente_alerta(String incidente_alerta) {
+        this.incidenteAlerta = incidente_alerta;
+    }
+}
