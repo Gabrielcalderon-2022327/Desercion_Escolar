ede44ce023726edfbd7e5d79839bead99115a34c
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-02-25 08:34:29 -0600

    feat: Se agrego carateristica para correoUsuario

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/repository/UsuarioRepository.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/repository/UsuarioRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Component
 public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
-
+    Optional<Usuario> findByCorreoUsuario(String correoUsuario);
 }
