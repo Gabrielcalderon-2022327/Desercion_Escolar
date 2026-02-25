f19ffd5f696a23d93a4387570bd11899a10d452c
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-02-25 09:04:08 -0600

    feat: Se agrego el controller de la entidad de Grado

---
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/GradoController.java
@@ -0,0 +1,56 @@
+package com.scrum.ProyectoDesercion.controller;
+
+import com.scrum.ProyectoDesercion.entity.Grado;
+import com.scrum.ProyectoDesercion.service.GradoService;
+import com.scrum.ProyectoDesercion.validator.GradoValidator;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/grados")
+public class GradoController {
+
+    private final GradoService service;
+    private final GradoValidator validator;
+
+    public GradoController(GradoService service, GradoValidator validator) {
+        this.service = service;
+        this.validator = validator;
+    }
+
+    @GetMapping
+    public List<Grado> getAllGrados(){
+        return service.getAllGrado();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getGradoById(@PathVariable Integer id){
+        Grado searchedGrado = service.getGradoById(id);
+        return new ResponseEntity<>(searchedGrado, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createGrado(@Valid @RequestBody Grado grado){
+        validator.validar(grado);
+        Grado createdGrado = service.saveGrado(grado);
+        return new ResponseEntity<>(createdGrado, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateGrado(@Valid @RequestBody Grado grado, @PathVariable Integer id){
+        validator.validar(grado);
+        Grado updatedGrado = service.updateGrado(id, grado);
+        return new ResponseEntity<>(updatedGrado, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteGrado(@PathVariable Integer id){
+        service.deleteGrado(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
