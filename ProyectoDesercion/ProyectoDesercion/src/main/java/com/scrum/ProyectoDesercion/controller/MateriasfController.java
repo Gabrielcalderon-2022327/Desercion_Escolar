298eefe15a9f6c78dc88a06ca28775476d4303df
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-02-25 08:28:04 -0600

    test: Se actualizo el controller

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/MateriasfController.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/MateriasfController.java
@@ -1,4 +1,5 @@
 package com.scrum.ProyectoDesercion.controller;
+
 import com.scrum.ProyectoDesercion.entity.MateriasF;
 import com.scrum.ProyectoDesercion.service.MateriasfService;
 import com.scrum.ProyectoDesercion.validator.MateriasFValidator;
@@ -12,41 +13,53 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/materiasf")
 public class MateriasfController {
+
     private final MateriasfService materiasfService;
     private final MateriasFValidator validator;
 
-    public MateriasfController(MateriasfService materiasfService, MateriasFValidator validator) {
+    public MateriasfController(MateriasfService materiasfService,
+                               MateriasFValidator validator) {
         this.materiasfService = materiasfService;
         this.validator = validator;
     }
+
     @GetMapping
-    public List<MateriasF> getAllMateriasF(){return materiasfService.getAllMateriasF();}
+    public List<MateriasF> getAllMateriasF() {
+        return materiasfService.getAllMateriasF();
+    }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> getMateriasFById(@PathVariable int id) {
-        MateriasF searchedMateriasF = materiasfService.getMateriasFById(id);
-        return new ResponseEntity<>(searchedMateriasF, HttpStatus.OK);
+    public ResponseEntity<MateriasF> getMateriasFById(@PathVariable Integer id) {
+        MateriasF materia = validator.validarMateriaExistente(id);
+
+        return ResponseEntity.ok(materia);
     }
 
     @PostMapping
-    public ResponseEntity<Object> createMateriasF(@Valid @RequestBody MateriasF materiasF){
+    public ResponseEntity<MateriasF> createMateriasF(@Valid @RequestBody MateriasF materiasF) {
 
-            validator.validar(materiasF);
-            MateriasF created = materiasfService.saveMateriasF(materiasF);
-            return ResponseEntity.status(HttpStatus.CREATED).body(created);
+        validator.validarRegistro(materiasF);
 
+        MateriasF created = materiasfService.saveMateriasF(materiasF);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
+
     @PutMapping("/{id}")
     public ResponseEntity<MateriasF> updateMateriasF(@PathVariable Integer id, @Valid @RequestBody MateriasF materiasF) {
-            validator.validar(materiasF);
-            MateriasF updated = materiasfService.updateMateriasF(id, materiasF);
-            return ResponseEntity.ok(updated);
+        validator.validarMateriaExistente(id);
 
+        MateriasF updated = materiasfService.updateMateriasF(id, materiasF);
+
+        return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteMateriasF(@PathVariable Integer id) {
+        validator.validarMateriaExistente(id);
+
         materiasfService.deleteMateriasF(id);
-        return ResponseEntity.ok("Materia eliminado con éxito");
+
+        return ResponseEntity.ok("Materia eliminada con éxito");
     }
 }
