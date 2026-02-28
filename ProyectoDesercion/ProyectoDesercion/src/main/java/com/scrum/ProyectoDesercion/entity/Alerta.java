d5a2d46f4d119bc5b461b09d090f2bf2b8092100
JeremyAquino-2025171 <jaquino-2025171@kinal.edu.gt>
2026-02-28 17:54:49 -0600

    fix: se agrego el JasonFormat a los metodos para que funcionaran

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/entity/Alerta.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/entity/Alerta.java
@@ -1,10 +1,11 @@
 package com.scrum.ProyectoDesercion.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
-
 import java.time.LocalDate;
 
 @Entity
@@ -12,24 +13,40 @@ import java.time.LocalDate;
 public class Alerta {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id_empleado")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_alerta")
     private Integer idAlerta;
 
     @Column(name = "fecha_alerta")
     @NotNull(message = "La fecha no puede ser nula")
     @Past(message = "Error: la fecha no puede ser futura")
+    @JsonProperty("fecha_alerta")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate fechaAlerta;
 
     @Column(name = "tipo_alerta")
     @NotBlank(message = "El tipo de alerta no puede estar vacío")
+    @JsonProperty("tipo_alerta")
     private String tipoAlerta;
 
     @Column(name = "incidente_alerta")
     @NotBlank(message = "El incidente de alerta no puede estar vacío")
+    @JsonProperty("incidente_alerta")
     private String incidenteAlerta;
 
-    // Getters y Setters
+    @Column(name = "fk_id_riesgo")
+    @NotNull(message = "El ID de riesgo es obligatorio")
+    @JsonProperty("fk_id_riesgo")
+    private Integer fkIdRiesgo;
+
+    @Column(name = "id_empleado")
+    @NotNull(message = "El ID del empleado es obligatorio")
+    @JsonProperty("id_empleado")
+    private Integer idEmpleado;
+
+    public Alerta() {}
+
+    // --- Getters y Setters ---
     public Integer getIdAlerta() { return idAlerta; }
     public void setIdAlerta(Integer idAlerta) { this.idAlerta = idAlerta; }
 
@@ -41,4 +58,10 @@ public class Alerta {
 
     public String getIncidenteAlerta() { return incidenteAlerta; }
     public void setIncidenteAlerta(String incidenteAlerta) { this.incidenteAlerta = incidenteAlerta; }
+
+    public Integer getFkIdRiesgo() { return fkIdRiesgo; }
+    public void setFkIdRiesgo(Integer fkIdRiesgo) { this.fkIdRiesgo = fkIdRiesgo; }
+
+    public Integer getIdEmpleado() { return idEmpleado; }
+    public void setIdEmpleado(Integer idEmpleado) { this.idEmpleado = idEmpleado; }
 }
