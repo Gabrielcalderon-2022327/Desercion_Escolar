ab69b2d4340765c1dee9a972d564ffa181c41c92
JeremyAquino-2025171 <jaquino-2025171@kinal.edu.gt>
2026-02-28 12:58:08 -0600

    feat: re configure el ProyectoDesercionApplication.java

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/ProyectoDesercionApplication.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/ProyectoDesercionApplication.java
@@ -1,13 +1,18 @@
 package com.scrum.ProyectoDesercion;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoDesercionApplication {
+public class ProyectoDesercionApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoDesercionApplication.class, args);
 	}
 
+	@Override
+	public void run (String... args) throws Exception{
+	System.out.println("api funcionando");
+	}
 }
