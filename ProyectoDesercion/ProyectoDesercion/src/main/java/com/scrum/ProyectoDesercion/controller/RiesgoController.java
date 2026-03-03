294a850e36138db4398e49088ca4600a83448d17
JeremyAquino-2025171 <jaquino-2025171@kinal.edu.gt>
2026-03-02 20:01:05 -0600

    fix: se mejoro el codigo para ser menos extenso

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/RiesgoController.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/RiesgoController.java
@@ -2,7 +2,7 @@ package com.scrum.ProyectoDesercion.controller;
 
 import com.scrum.ProyectoDesercion.entity.Riesgo;
 import com.scrum.ProyectoDesercion.service.RiesgoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,42 +13,37 @@ import java.util.List;
 @RequestMapping("/api/riesgo")
 public class RiesgoController {
 
-    @Autowired
-    private RiesgoService riesgoService;
+    private final RiesgoService riesgoService;
+
+    public RiesgoController(RiesgoService riesgoService) {
+        this.riesgoService = riesgoService;
+    }
 
     @GetMapping
     public ResponseEntity<List<Riesgo>> listarRiesgos() {
         return new ResponseEntity<>(riesgoService.listarRiesgos(), HttpStatus.OK);
     }
-
     @GetMapping("/{id}")
-    public ResponseEntity<Riesgo> buscarRiesgo(@PathVariable Integer id) {
-        Riesgo riesgo = riesgoService.buscarRiesgo(id);
-        if (riesgo != null) {
-            return new ResponseEntity<>(riesgo, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> buscarPorId(@PathVariable Integer id) {
+        Riesgo searchedRiesgo = riesgoService.buscarRiesgo(id);
+        return new ResponseEntity<>(searchedRiesgo, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Riesgo> crearRiesgo(@RequestBody Riesgo riesgo) {
-        Riesgo nuevoRiesgo = riesgoService.crearRiesgo(riesgo);
-        return new ResponseEntity<>(nuevoRiesgo, HttpStatus.CREATED);
+    public ResponseEntity<Object> createRiesgo(@Valid @RequestBody Riesgo riesgo) {
+        Riesgo createdRiesgo = riesgoService.crearRiesgo(riesgo);
+        return new ResponseEntity<>(createdRiesgo, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Riesgo> actualizarRiesgo(@PathVariable Integer id, @RequestBody Riesgo riesgo) {
-        try {
-            Riesgo riesgoActualizado = riesgoService.actualizarRiesgo(id, riesgo);
-            return new ResponseEntity<>(riesgoActualizado, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<Object> updateRiesgo(@PathVariable Integer id, @Valid @RequestBody Riesgo riesgo) {
+        Riesgo updatedRiesgo = riesgoService.actualizarRiesgo(id, riesgo);
+        return ResponseEntity.ok(updatedRiesgo);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminarRiesgo(@PathVariable Integer id) {
+    public ResponseEntity<Object> deleteRiesgo(@PathVariable Integer id) {
         riesgoService.eliminarRiesgo(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT); // Devuelve 204 sin contenido
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
