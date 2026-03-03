d4bcf3d2430272a41efede361d63de1449a872df
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-03-02 19:17:04 -0600

    feat: Se agregaron las validaciones a la entity de Usuario

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/entity/Usuario.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/entity/Usuario.java
@@ -1,6 +1,9 @@
 package com.scrum.ProyectoDesercion.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.NotNull;
 
 import java.sql.Date;
 
@@ -13,15 +16,20 @@ public class Usuario {
     private Integer idUsuario;
 
     @Column(name = "correo_usuario")
+    @NotBlank(message = "El correo es un campo obligatorio")
     private String correoUsuario;
 
     @Column(name = "contra_usuario")
+    @NotBlank(message = "La contraseña es un campo obligatorio")
     private String contraUsuario;
 
     @Column(name = "rol_usuario")
+    @NotBlank(message = "El rol de usuario es un campo obligatorio")
     private String rolUsuario;
 
     @Column(name = "creacion_usuario")
+    @NotNull(message = "La fecha de creacion es un campo obligatorio")
+    @Past(message = "La fecha no puede ser futura")
     private Date creacionUsuario;
 
     //Generar getter and setter a todos
