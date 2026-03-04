dbbedfe9367d759a063103b7a31e21b9ae03a3ae
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-03-03 18:27:35 -0600

    fix: Se arreglaron problemas con la configuracion

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/exception/GlobalExceptionHandler.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/exception/GlobalExceptionHandler.java
@@ -21,35 +21,27 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<Object> validarErroresConstraint(SQLIntegrityConstraintViolationException ex){
+    public ResponseEntity<Object> validarErroresConstraint(SQLIntegrityConstraintViolationException ex) {
         return ResponseEntity.badRequest().body(Map.of("Error", "No existe esa llave foranea"));
     }
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<Object> validarJSON(HttpMediaTypeNotSupportedException ex){
+    public ResponseEntity<Object> validarJSON(HttpMediaTypeNotSupportedException ex) {
         return ResponseEntity.badRequest().body(Map.of("Error", "El archivo no es tipo JSON"));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Object> validarTipoDato(){
+    public ResponseEntity<Object> validarTipoDato() {
         return ResponseEntity.badRequest().body(Map.of("Error", "El tipo de dato esta incorrecto o hay mala estructura en el JSON"));
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> captarThrows(ResourceNotFoundException ex){
+    public ResponseEntity<?> captarThrows(ResourceNotFoundException ex) {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> validacionesNegocio(IllegalArgumentException ex){
+    public ResponseEntity<Object> validacionesNegocio(IllegalArgumentException ex) {
         return ResponseEntity.badRequest().body(Map.of("Error", ex.getMessage()));
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 3c84bceabbce306173aad04f1d9cd4a60fa59f31
-=======
-}
->>>>>>> a7964f992b64ed4a900de1650dc1d527c7717a06
