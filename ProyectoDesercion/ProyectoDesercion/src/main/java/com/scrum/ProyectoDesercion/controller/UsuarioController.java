f07fe1c38fbc7e2094d55b049bc1477e4c6521e1
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-04-20 15:54:34 -0600

    feat: Se cambio el Controller de la entidad Usuario para que conectar el frontend con su CRUD completo

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/UsuarioController.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/UsuarioController.java
@@ -4,16 +4,16 @@ import com.scrum.ProyectoDesercion.entity.Usuario;
 import com.scrum.ProyectoDesercion.service.UsuarioService;
 import com.scrum.ProyectoDesercion.validator.UsuarioValidator;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/usuarios")
+@Controller
 public class UsuarioController {
-
     private final UsuarioService usuarioService;
     private final UsuarioValidator validator;
 
@@ -22,63 +22,59 @@ public class UsuarioController {
         this.validator = validator;
     }
 
-    @GetMapping
-    public List<Usuario> getAllUsuarios() {
-        return usuarioService.getAllUsuarios();
-    }
-
-    @PostMapping
-    public ResponseEntity<Object> createUsuario(@Valid @RequestBody Usuario usuario) {
-
-        validator.validarRegistro(usuario);  
-
-        Usuario created = usuarioService.saveUsuario(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(created);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getUsuarioById(@PathVariable int id) {
-
-        Usuario searchedUsuario = usuarioService.getUsuarioById(id);
-
-        if (searchedUsuario == null) {
-            throw new IllegalArgumentException("Usuario no encontrado con ID: " + id);
+    @GetMapping("/usuarios")
+    public String cargarUsuarios(Model model){
+        if(!model.containsAttribute("usuarios")){
+            model.addAttribute("usuarios", usuarioService.getAllUsuarios());
         }
-
-        return ResponseEntity.ok(searchedUsuario);
+        return "Usuario";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Usuario> updateUsuario(
-            @PathVariable Integer id,
-            @Valid @RequestBody Usuario usuario) {
-
-        Usuario usuarioExistente = usuarioService.getUsuarioById(id);
-
-        if (usuarioExistente == null) {
-            throw new IllegalArgumentException("Usuario no encontrado con ID: " + id);
-        }
-
-        usuario.setIdUsuario(id);
-
-        Usuario updated = usuarioService.updateUsuario(id, usuario);
-
-        return ResponseEntity.ok(updated);
+    @GetMapping("/usuarios/listar")
+    public String listarUsuarios(RedirectAttributes redirectAttributes){
+        redirectAttributes.addFlashAttribute("usuarios", usuarioService.getAllUsuarios());
+        return "redirect:/usuarios";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteUsuario(@PathVariable Integer id) {
+    @GetMapping("/usuarios/buscar")
+    public String buscarUsuario(RedirectAttributes redirectAttributes, @RequestParam Integer idUsuario){
+        Usuario usuario = usuarioService.getUsuarioById(idUsuario);
+        redirectAttributes.addFlashAttribute("usuarios", List.of(usuario));
+        return "redirect:/usuarios";
+    }
 
-        Usuario usuarioExistente = usuarioService.getUsuarioById(id);
+    @PostMapping("/usuarios/crear")
+    public String crearUsuario( @Valid @RequestParam String correoUsuario,
+                                @Valid @RequestParam String contraUsuario,
+                                @Valid @RequestParam String rolUsuario){
+        Usuario newUsuario = new Usuario();
+        newUsuario.setCorreoUsuario(correoUsuario);
+        newUsuario.setContraUsuario(contraUsuario);
+        newUsuario.setRolUsuario(rolUsuario);
+        newUsuario.setCreacionUsuario(LocalDate.now());
+        validator.validarRegistro(newUsuario);
+        usuarioService.saveUsuario(newUsuario);
+        return "redirect:/usuarios";
+    }
 
-        if (usuarioExistente == null) {
-            throw new IllegalArgumentException("Usuario no encontrado con ID: " + id);
-        }
+    @PostMapping("/usuarios/editar")
+    public String editarUsuario(@Valid @RequestParam Integer idUsuario,
+                                @Valid @RequestParam String correoUsuario,
+                                @Valid @RequestParam String contraUsuario,
+                                @Valid @RequestParam String rolUsuario,
+                                @Valid @RequestParam LocalDate creacionUsuario){
+        Usuario newUsuario = new Usuario();
+        newUsuario.setCorreoUsuario(correoUsuario);
+        newUsuario.setContraUsuario(contraUsuario);
+        newUsuario.setRolUsuario(rolUsuario);
+        newUsuario.setCreacionUsuario(creacionUsuario);
+        usuarioService.updateUsuario(idUsuario,newUsuario);
+        return "redirect:/usuarios";
+    }
 
+    @GetMapping("/usuarios/eliminar/{id}")
+    public String eliminarUsuario(@PathVariable Integer id){
         usuarioService.deleteUsuario(id);
-
-        return ResponseEntity.ok("Usuario eliminado con éxito");
+        return "redirect:/usuarios";
     }
 }
-
-
