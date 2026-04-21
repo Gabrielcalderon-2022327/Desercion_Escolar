8a819b30425c357332878058a3e0f666d48e4dcd
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-04-20 21:51:41 -0600

    feat: Se agregaron todos los cruds funcionales para la entidad Alerta

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/AlertaController.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/AlertaController.java
@@ -2,50 +2,81 @@ package com.scrum.ProyectoDesercion.controller;
 
 import com.scrum.ProyectoDesercion.entity.Alerta;
 import com.scrum.ProyectoDesercion.service.AlertaService;
+import com.scrum.ProyectoDesercion.validator.AlertaValidator;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/alerta")
+@Controller
 public class AlertaController {
 
-    private final AlertaService alertaService;
+    @Autowired
+    private AlertaService alertaService;
+    @Autowired
+    private AlertaValidator alertaValidator;
 
-    public AlertaController(AlertaService alertaService) {
-        this.alertaService = alertaService;
-    }
-
-
-    @GetMapping
-    public List<Alerta> getAllAlerta() {
-        return alertaService.getAllAlerta();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> buscarPorId(@PathVariable Integer id) {
-        Alerta searchedAlerta = alertaService.getAlertaById(id);
-        return new ResponseEntity<>(searchedAlerta, HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<Object> createAlerta(@Valid @RequestBody Alerta alerta) {
-            Alerta createdAlerta = alertaService.saveAlerta(alerta);
-            return new ResponseEntity<>(createdAlerta, HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateAlerta(@PathVariable Integer id, @Valid @RequestBody Alerta alerta) {
-            Alerta updatedAlerta = alertaService.updateAlerta(id, alerta);
-            return ResponseEntity.ok(updatedAlerta);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteAlerta(@PathVariable Integer id) {
-            alertaService.deleteAlerta(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @GetMapping("/alertas")
+    public String cargarAlertas(Model model){
+        if(!model.containsAttribute("alertas")){
+            model.addAttribute("alertas", alertaService.getAllAlerta());
         }
+        return "Alerta";
+    }
+
+    @GetMapping("/alertas/listar")
+    public String listarAlertas(RedirectAttributes redirectAttributes){
+        redirectAttributes.addFlashAttribute("alertas", alertaService.getAllAlerta());
+        return "redirect:/alertas";
+    }
+
+    @GetMapping("/alertas/buscar")
+    public String buscarAlertas(RedirectAttributes redirectAttributes, @RequestParam Integer idAlerta){
+        Alerta alerta = alertaService.getAlertaById(idAlerta);
+        redirectAttributes.addFlashAttribute("alertas", List.of(alerta));
+        return "redirect:/alertas";
+    }
+
+    @PostMapping("/alertas/crear")
+    public String crearAlerta(@Valid @RequestParam LocalDate fecha_alerta,
+                              @Valid @RequestParam String tipo_alerta,
+                              @Valid @RequestParam String incidente_alerta,
+                              @Valid @RequestParam Integer fk_id_riesgo){
+        Alerta newAlerta = new Alerta();
+        newAlerta.setFecha_alerta(fecha_alerta);
+        newAlerta.setTipo_alerta(tipo_alerta);
+        newAlerta.setIncidente_alerta(incidente_alerta);
+        newAlerta.setFk_id_riesgo(fk_id_riesgo);
+        alertaValidator.validar(newAlerta);
+        alertaService.saveAlerta(newAlerta);
+        return "redirect:/alertas";
+    }
+
+    @PostMapping("/alertas/editar")
+    public String editarAlerta( @Valid @RequestParam Integer id_alerta,
+                                @Valid @RequestParam LocalDate fecha_alerta,
+                                @Valid @RequestParam String tipo_alerta,
+                                @Valid @RequestParam String incidente_alerta,
+                                @Valid @RequestParam Integer fk_id_riesgo){
+        Alerta newAlerta = new Alerta();
+        newAlerta.setFecha_alerta(fecha_alerta);
+        newAlerta.setTipo_alerta(tipo_alerta);
+        newAlerta.setIncidente_alerta(incidente_alerta);
+        newAlerta.setFk_id_riesgo(fk_id_riesgo);
+        alertaService.updateAlerta(id_alerta, newAlerta);
+        return "redirect:/alertas";
+    }
+
+    @GetMapping("/alertas/eliminar/{id}")
+    public String eliminarAlerta(@PathVariable Integer id){
+        alertaService.deleteAlerta(id);
+        return "redirect:/alertas";
+    }
+
+
 }
