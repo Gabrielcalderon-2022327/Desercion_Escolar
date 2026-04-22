042afce1b43deae9f69bcfd97df28eebdc230a25
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-04-22 08:25:28 -0600

    add:Se hizo el controlador para la pagina principal

---
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/IndexController.java
@@ -0,0 +1,64 @@
+package com.scrum.ProyectoDesercion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    // Página principal (Dashboard)
+    @GetMapping({"/", "/paginaprincipal"})
+    public String dashboard() {
+        return "Pagina principal";
+    }
+
+    @GetMapping("/estudiante")
+    public String estudiantes() {
+        return "Estudiantes";
+    }
+
+    @GetMapping("/encargados")
+    public String encargados() {
+        return "Encargado";
+    }
+
+    @GetMapping("/grados")
+    public String grados() {
+        return "Grado";
+    }
+
+    @GetMapping("/maestros")
+    public String maestros() {
+        return "Maestro";
+    }
+
+    @GetMapping("/usuarios")
+    public String usuarios() {
+        return "Usuario";
+    }
+
+    @GetMapping("/asistencias")
+    public String asistencias() {
+        return "Asistencia";
+    }
+
+    @GetMapping("/materias-f")
+    public String materiasF() {
+        return "MateriasF";
+    }
+
+    @GetMapping("/economia")
+    public String economia() {
+        return "Economia";
+    }
+
+    @GetMapping("/riesgo")
+    public String riesgo() {
+        return "Riesgo";
+    }
+
+    @GetMapping("/alertas")
+    public String alertas() {
+        return "Alerta";
+    }
+}
