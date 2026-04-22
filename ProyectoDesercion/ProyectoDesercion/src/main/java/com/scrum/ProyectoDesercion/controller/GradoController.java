6c009180982d8d7b3df046adf0b93fa5c8846210
Jeremy <jaquino-2025171@kinal.edu.gt>
2026-04-22 10:17:48 -0600

    feat: el controller ya esta terminado con todas las referencias completas

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/GradoController.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/controller/GradoController.java
@@ -3,16 +3,16 @@ package com.scrum.ProyectoDesercion.controller;
 import com.scrum.ProyectoDesercion.entity.Grado;
 import com.scrum.ProyectoDesercion.service.GradoService;
 import com.scrum.ProyectoDesercion.validator.GradoValidator;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 
-@RestController
-@RequestMapping("/api/grados")
+@Controller
+@RequestMapping("/grados")
 public class GradoController {
 
     private final GradoService service;
@@ -23,34 +23,57 @@ public class GradoController {
         this.validator = validator;
     }
 
-    @GetMapping
-    public List<Grado> getAllGrados(){
-        return service.getAllGrado();
+    @GetMapping("/")
+    public String cargarGrados(Model model) {
+        if (!model.containsAttribute("grados")) {
+            model.addAttribute("grados", service.getAllGrado());
+        }
+        return "Grado";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getGradoById(@PathVariable Integer id){
-        Grado searchedGrado = service.getGradoById(id);
-        return new ResponseEntity<>(searchedGrado, HttpStatus.OK);
+    @GetMapping("/buscar")
+    public String buscarGrado(RedirectAttributes redirectAttributes, @RequestParam Integer idUsuario) {
+        Grado grado = service.getGradoById(idUsuario);
+        redirectAttributes.addFlashAttribute("grados", List.of(grado));
+        return "redirect:/grados/";
     }
 
-    @PostMapping
-    public ResponseEntity<Object> createGrado(@Valid @RequestBody Grado grado){
-        validator.validar(grado);
-        Grado createdGrado = service.saveGrado(grado);
-        return new ResponseEntity<>(createdGrado, HttpStatus.CREATED);
+
+    @PostMapping("/crear")
+    public String crearGrado(
+            @RequestParam String nombre_grado,
+            @RequestParam Integer fk_id_maestro) {
+
+        Grado nuevoGrado = new Grado();
+        nuevoGrado.setNombre_grado(nombre_grado);
+        nuevoGrado.setFk_id_maestro(fk_id_maestro);
+
+        validator.validar(nuevoGrado);
+        service.saveGrado(nuevoGrado);
+        return "redirect:/grados/";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateGrado(@Valid @RequestBody Grado grado, @PathVariable Integer id){
-        validator.validar(grado);
-        Grado updatedGrado = service.updateGrado(id, grado);
-        return new ResponseEntity<>(updatedGrado, HttpStatus.OK);
+
+    @PostMapping("/editar")
+    public String editarGrado(
+            @RequestParam Integer id_grado,
+            @RequestParam String nombre_grado,
+            @RequestParam Integer fk_id_maestro) {
+
+        Grado nuevoGrado = new Grado();
+        nuevoGrado.setId_grado(id_grado);
+        nuevoGrado.setNombre_grado(nombre_grado);
+        nuevoGrado.setFk_id_maestro(fk_id_maestro);
+
+        validator.validar(nuevoGrado);
+        service.updateGrado(id_grado, nuevoGrado);
+        return "redirect:/grados/";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteGrado(@PathVariable Integer id){
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarGrado(@PathVariable Integer id) {
         service.deleteGrado(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return "redirect:/grados/";
     }
 }
