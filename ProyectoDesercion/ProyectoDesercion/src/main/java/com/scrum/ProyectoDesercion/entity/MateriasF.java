8025e2cc5f64c9200d9f4a501d4cb63bfb9b7cde
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-04-28 14:45:54 -0600

    feat: Se agrega una referencia con la tabla Maestro y Estudiante para acceder a sus datos

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/entity/MateriasF.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/entity/MateriasF.java
@@ -2,7 +2,6 @@ package com.scrum.ProyectoDesercion.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
@@ -24,66 +23,57 @@ public class MateriasF {
     @Column(name = "descripcion_materiaf")
     @NotBlank(message = "La descripcion es un campo obligatorio")
     private String descripcionMateriaF;
-    
+
     @Column(name = "fecha_alerta_materiaf")
     @NotNull(message = "La fecha no puede ser nula")
-    @Past(message = "Error: la fecha no puede ser futura")
     @PastOrPresent(message = "Error: la fecha no puede ser futura")
     private LocalDate fechaAlertaMateriaF;
 
     @Column(name = "fk_id_maestro")
-    @NotNull(message = "El id del maestro es un campo  obligatorio")
+    @NotNull(message = "El id del maestro es obligatorio")
     private Integer idMaestro;
 
+    @ManyToOne
+    @JoinColumn(name = "fk_id_maestro", insertable = false, updatable = false)
+    private Maestro maestro;
+
     @Column(name = "fk_id_estudiante")
-    @NotNull(message = "El id del estudiante es un campo  obligatorio")
+    @NotNull(message = "El id del estudiante es obligatorio")
     private Integer idEstudiante;
 
-    //Generar getter and setter a todos
+    @ManyToOne
+    @JoinColumn(name = "fk_id_estudiante", insertable = false, updatable = false)
+    private Estudiante estudiante;
 
-    public Integer getIdMateriasF() {
-        return idMateriasF;
-    }
+    public Integer getIdMateriasF() {return idMateriasF;}
 
-    public void setIdMateriasF(Integer idMateriasF) {
-        this.idMateriasF = idMateriasF;
-    }
+    public void setIdMateriasF(Integer idMateriasF) {this.idMateriasF = idMateriasF;}
 
-    public String getNombreMateriaF() {
-        return nombreMateriaF;
-    }
+    public String getNombreMateriaF() {return nombreMateriaF;}
 
-    public void setNombreMateriaF(String nombreMateriaF) {
-        this.nombreMateriaF = nombreMateriaF;
-    }
+    public void setNombreMateriaF(String nombreMateriaF) {this.nombreMateriaF = nombreMateriaF;}
 
-    public String getDescripcionMateriaF() {
-        return descripcionMateriaF;
-    }
+    public String getDescripcionMateriaF() {return descripcionMateriaF;}
 
-    public void setDescripcionMateriaF(String descripcionMateriaF) {
-        this.descripcionMateriaF = descripcionMateriaF;
-    }
+    public void setDescripcionMateriaF(String descripcionMateriaF) {this.descripcionMateriaF = descripcionMateriaF;}
 
-    public LocalDate getFechaAlertaMateriaF() {
-        return fechaAlertaMateriaF;
-    }
+    public LocalDate getFechaAlertaMateriaF() {return fechaAlertaMateriaF;}
 
     public void setFechaAlertaMateriaF(LocalDate fechaAlertaMateriaF) {this.fechaAlertaMateriaF = fechaAlertaMateriaF;}
 
-    public Integer getIdMaestro() {
-        return idMaestro;
-    }
+    public Integer getIdMaestro() {return idMaestro;}
 
-    public void setIdMaestro(Integer idMaestro) {
-        this.idMaestro = idMaestro;
-    }
+    public void setIdMaestro(Integer idMaestro) {this.idMaestro = idMaestro;}
 
-    public Integer getIdEstudiante() {
-        return idEstudiante;
-    }
+    public Integer getIdEstudiante() {return idEstudiante;}
 
-    public void setIdEstudiante(Integer idEstudiante) {
-        this.idEstudiante = idEstudiante;
-    }
+    public void setIdEstudiante(Integer idEstudiante) {this.idEstudiante = idEstudiante;}
+
+    public Maestro getMaestro() {return maestro;}
+
+    public void setMaestro(Maestro maestro) {this.maestro = maestro;}
+
+    public Estudiante getEstudiante() {return estudiante;}
+
+    public void setEstudiante(Estudiante estudiante) {this.estudiante = estudiante;}
 }
