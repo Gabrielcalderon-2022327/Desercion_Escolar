ccbaadd80aa40e92fe465baebfcdedc1125c0cc6
Gabrielcalderon-2022327 <gcalderon-2022327>
2026-04-29 08:07:43 -0600

    feat: Se agrego funcion que permite buscar por nombre de maestro

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/repository/MaestroRepository.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/repository/MaestroRepository.java
@@ -3,5 +3,8 @@ package com.scrum.ProyectoDesercion.repository;
 import com.scrum.ProyectoDesercion.entity.Maestro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MaestroRepository extends JpaRepository<Maestro,Integer> {
+    Optional<Maestro> findByNombreMaestro(String nombreMaestro);
 }
