250f6e62c2adfdbd51fe70c65fcaf650e7c90b5f
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-05-04 22:06:14 -0600

    feat: Se cambio el service y se conviertio en una mini app para tomar asistencia

--- ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/service/AsistenciaService.java
+++ ProyectoDesercion/ProyectoDesercion/src/main/java/com/scrum/ProyectoDesercion/service/AsistenciaService.java
@@ -2,13 +2,13 @@ package com.scrum.ProyectoDesercion.service;
 import com.scrum.ProyectoDesercion.entity.Asistencia;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public interface AsistenciaService {
     List<Asistencia> getAllAsistencia();
-    Asistencia getAsistenciaById(Integer id);
-    Asistencia saveAsistencia(Asistencia asistencia) throws RuntimeException;
-    Asistencia updateAsistencia(Integer id, Asistencia asistencia);
-    void deleteAsistencia(Integer id);
+    List<Asistencia> getByFechaGrado(LocalDate fechaAsistencia, Integer id_grado);
+    void saveAll(List<Asistencia> asistencias);
+
 }
